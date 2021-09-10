57afc86a46692e7bd7b82934b589bdd57cf5693d
Ryan Vu <ryanvu2003@gmail.com>
2021-09-09 21:01:30 -0400

    get lessons route

--- routes/lessonRoute.js
+++ routes/lessonRoute.js
@@ -4,7 +4,15 @@ const sql = require("../db/index");
 
 router.get("/:user_id", async (req, res) => {
   try {
-    res.send(req.params.user_id);
+    const userLessons = await sql`
+        select * from lessons where teacher = ${req.params.user_id} or student = ${req.params.user_id}
+    `;
+
+    if (userLessons.length <= 0) {
+      res.send("You have no lessons planned");
+    } else {
+      res.send(userLessons);
+    }
   } catch (error) {
     console.log(error);
     res.send(error);
